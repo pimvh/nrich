aac3b11b40c75cb50e4f0a9293a24ef96d8db89f
Pim van Helvoirt <pim.van.helvoirt@home.nl>
2022-05-19 16:50:24 +0200

    resolved bug in parse_internetdb. some errors occured using it

--- parse_internetdb.py
+++ parse_internetdb.py
@@ -4,6 +4,9 @@ import json
 import pprint
 import sys
 
+
+PORTS_OF_INTEREST = {3389}
+
 def main():
     """ program made to be able to parse shodan internet db output, which looks like this:
         {
@@ -18,27 +21,27 @@ def main():
     """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-input-file', '-i', type=argparse.FileType('r'), 
-                        default=(None if sys.stdin.isatty() else sys.stdin),
+    parser.add_argument('-input-file', '-i', type=str,
+                        default="out.json",
                         help="File to parse IPs from, if not given parse from stdin")
-    parser.add_argument('-output-file', '-o', type=argparse.FileType('w+'), help="file to write IPs to",
-                        default=None)
+    # parser.add_argument('-output-file', '-o', type=argparse.FileType('w+'), help="file to write IPs to", default=None)
+    args = parser.parse_args()
 
-    with open('out.json') as f:
+    with open(args.input_file, 'r') as f:
         shodan = json.load(f)
 
     for i, item in enumerate(shodan):
 
         if item.get('vulns'):
-            pprint.pprint(item, '\n')
+            pprint.pprint(item)
 
-        if ip := item.get('ip'):
+        elif ip := item.get('ip'):
             if ip == 'YOUR IP':
-               pprint.pprint(item, '\n')
+               pprint.pprint(item)
 
-        if ports := item.get('ports'):
-            if 3389 in ports:
-               pprint.pprint(item, '\n')
+        elif ports := item.get('ports'):
+            if PORTS_OF_INTEREST & set(ports):
+                pprint.pprint(item)
 
 if __name__ == "__main__":
     main()
